cae6c17d93dc69b75839323a9c8dfd1849ee6bfe
KINARI NISHIYAMA <KINARI@nishiyamaseisei-no-MacBook-Air.local>
2015-04-17 14:45:18 +0900

    Initial Commit

---
+++ TouchStamp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchStamp
+//
+//  Created by KINARI NISHIYAMA on 2015/04/17.
+//  Copyright (c) 2015年 KINARI NISHIYAMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchStamp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchStamp
+//
+//  Created by KINARI NISHIYAMA on 2015/04/17.
+//  Copyright (c) 2015年 KINARI NISHIYAMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
