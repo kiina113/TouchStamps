2aba6cc490e7f096d32200481aa890a269c8b87c
KINARI NISHIYAMA <KINARI@nishiyamaseisei-no-MacBook-Air.local>
2015-04-19 18:29:29 +0900

    0419

--- TouchStamp/ViewController.h
+++ TouchStamp/ViewController.h
@@ -8,7 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    
+    NSString *ImageNamed[4];
+    int imageNumber;
+    UIImage *Image;
+}
+
 
 
 @end
--- TouchStamp/ViewController.m
+++ TouchStamp/ViewController.m
@@ -17,6 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //選択している画像の番号を0にする
+    imageNumber = 0;
+    
+    //配列に４枚の画像の名前を入れる
+    ImageNamed[0]=@"hana.png";
+    ImageNamed[1]=@"ハート,png";
+    ImageNamed[2]=@"usagi.png";
+    ImageNamed[3]=@"犬.png";
+    
+    //背景画像を設定する
+    Image = [UIImage imageNamed:@"幾何学.png"];    
+    
 }
 
 - (void)didReceiveMemoryWarning {
